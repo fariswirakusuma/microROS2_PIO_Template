a9f56afe2ca46e5b005b5dbbcab77fb96f8ba592
fariswirakusuma <fariswkt@gmail.com>
2026-03-07 15:52:52 +0700

    chore:modify .gitignore

--- raspberry_pi_ws/build/ngetestyahbot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntunya-faris/ITB/URO/ROS_Rasppi_to_STM_min-bluepill-/raspberry_pi_ws/ngetestyahbot
-# Build directory: /home/ubuntunya-faris/ITB/URO/ROS_Rasppi_to_STM_min-bluepill-/raspberry_pi_ws/build/ngetestyahbot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
